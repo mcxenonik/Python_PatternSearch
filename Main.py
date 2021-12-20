dfedd625d4cfe75f11f92b1acf5f29f06bfa3927
Igor Nowak <i.nowak@stud.elka.pw.edu.pl>
2021-12-20 04:36:23 +0100

    RKAlgorithm.py added

--- Main.py
+++ Main.py
@@ -3,6 +3,7 @@ from BrutalAlgorithm2 import BrutalAlgorithm2
 from KMPAlgorithm import KMPAlgorithm
 from BMAlgorithm import BMAlgorithm
 from BMAlgorithm2 import BMAlgorithm2
+from RKAlgorithm import RKAlgorithm
 from Utils import Utils
 
 def run(input_file_name, number_of_elements_list):
@@ -12,6 +13,8 @@ def run(input_file_name, number_of_elements_list):
 
     Utils.start_measurements(KMPAlgorithm, input_file_name, number_of_elements_list)
 
+    Utils.start_measurements(RKAlgorithm, input_file_name, number_of_elements_list)
+
     Utils.start_measurements(BMAlgorithm, input_file_name, number_of_elements_list)
 
     Utils.start_measurements(BMAlgorithm2, input_file_name, number_of_elements_list)
@@ -20,8 +23,8 @@ def run(input_file_name, number_of_elements_list):
 if __name__ == "__main__":
     input_file_name = "pan-tadeusz.txt"
     # number_of_elements_list = [n for n in range(1000, 68336, 5000)]            # MAX 68336
-    number_of_elements_list = [n for n in range(100, 5001, 100)]
-    # number_of_elements_list = [n for n in range(100, 2001, 100)]
+    # number_of_elements_list = [n for n in range(100, 5001, 100)]
+    number_of_elements_list = [n for n in range(100, 1501, 100)]
     # number_of_elements_list = [n for n in range(10, 15, 10)]
 
     run(input_file_name, number_of_elements_list)
---
+++ RKAlgorithm.py
@@ -0,0 +1,48 @@
+class RKAlgorithm:
+    def __init__(self):
+        pass
+
+
+    def __str__(self):
+        return "RKAlgorithm"
+
+
+    @staticmethod
+    def run_algorithm(pattern, text):
+        d = 95
+        q = 997
+
+        pattern_length = len(pattern)
+        text_length = len(text)
+        pattern_index = 0
+        text_index = 0
+        pattern_hash = 0
+        text_hash = 0
+        h = 1
+
+        for pattern_index in range(pattern_length - 1):
+            h = (h * d) % q
+
+        # Calculate hash value for pattern and text
+        for pattern_index in range(pattern_length):
+            pattern_hash = (d * pattern_hash + ord(pattern[pattern_index])) % q
+            text_hash = (d * text_hash + ord(text[pattern_index])) % q
+
+        # Find the match
+        for text_index in range(text_length - pattern_length + 1):
+            if (pattern_hash == text_hash):
+                for pattern_index in range(pattern_length):
+                    if (text[text_index + pattern_index] != pattern[pattern_index]):
+                        break
+
+                pattern_index += 1
+                if (pattern_index == pattern_length):
+                    return text_index + 1
+
+            if (text_index < text_length - pattern_length):
+                text_hash = (d * (text_hash - ord(text[text_index]) * h) + ord(text[text_index + pattern_length])) % q
+
+                if (text_hash < 0):
+                    text_hash += q
+
+        return None
